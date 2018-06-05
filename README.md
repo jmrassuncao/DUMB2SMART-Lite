c92e2d25f75e54716f0a2b5027abaa0acf2a7f99
Jorge Assunção <jorgeassuncao@users.noreply.github.com>
2018-06-05 16:25:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# DUMB2SMART
+
+The **DUMB2SMART Lite** is a way for some dumb equipment to gain some intelligence and inform a MQTT broker if they are ON or OFF. It was developed for use with ESP-01 in mind but can be used with other ESPs.
+
+### Version
+Current version is: _DUMB2SMART Lite v0.0.1 (05/06/2018)_
+
+### Features
+  - n/a
+
+### How it works
+The ESP-01 should be connected to the device you want to get the state.
+
+If you're using Home Assistant, just add this code:
+
+```yaml
+binary_sensor:
+#### GET DEVICE STATE
+  - platform: mqtt
+    name: "dumb2smart"
+    state_topic: "home/test/status"
+    value_template: "{{ value }}"
+    payload_on: "Online"
+    payload_off: "Offline"
+    device_class: connectivity
+```
+
+### Notes
+This project was created on PlatformIO. If you are using Arduino IDE, you should open the _.cpp_ file inside the _src_ folder. Libraries are inside the _lib_ folder.
+
+### Used LIBRARIES
+
+#### Arduino
+  - ESP8266WiFi >> https://github.com/esp8266/Arduino
+  - PubSubClient >> https://github.com/knolleary/pubsubclient
+
+#### PlatformIO
+  - ESP8266WiFi >> ```??```
+  - PubSubClient >> ```platformio lib install "PubSubClient"```
---
+++ src/dumb2smart_lite.cpp
@@ -0,0 +1,68 @@
+/**
+ *        _____  ____    ____  _______          _
+ *       |_   _||_   \  /   _||_   __ \        / \
+ *         | |    |   \/   |    | |__) |      / _ \
+ *     _   | |    | |\  /| |    |  __ /      / ___ \
+ *    | |__' |   _| |_\/_| |_  _| |  \ \_  _/ /   \ \_
+ *    `.____.'  |_____||_____||____| |___||____| |____|
+ *
+ * DUMB2SMART Lite by Jorge Assunção
+ */
+
+//************* INCLUDE LIBRARIES ************************************************************************
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+//************* PROJECT AND VERSION **********************************************************************
+const char* proj_ver = "DUMB2SMART (04/06/2018)";                               // Project name and version
+//************ CONFIG WIFI *******************************************************************************
+const char* WIFI_SSID = "MILKYWAY";                                             // Wifi SSID
+const char* WIFI_PASS = "2526F68597";                                           // Wifi password
+IPAddress ip(192,168,1,224);                                                    // IP address
+IPAddress gateway(192,168,1,1);                                                 // Gateway
+IPAddress subnet(255,255,255,0);                                                // Subnet mask
+#define DEVICE_HOSTNAME "teste_node"                                            // Hostname
+//************* CONFIG MQTT ******************************************************************************
+const char* MQTT_SERVER = "192.168.1.200";                                      // MQTT server address
+const char* MQTT_USERNAME = "homeassistant";                                    // MQTT server username
+const char* MQTT_PASSWORD = "JA2508ca72";                                       // MQTT server password
+const char* MQTT_ID = "test_node";                                              // Node ID
+const int MQTT_PORT = 1883;                                                     // MQTT server port
+//************ CONFIG MQTT LWT ***************************************************************************
+const char* MQTT_WILL_TOPIC = "home/test/status";                               // MQTT last will topic
+const char* MQTT_WILL_MESSAGE_ON = "Online";                                    // MQTT last will message on
+const char* MQTT_WILL_MESSAGE_OFF = "Offline";                                  // MQTT last will message off
+int MQTT_WILL_QOS = 1;                                                          // MQTT last will QoS (0,1 or 2)
+int MQTT_WILL_RETAIN = 1;                                                       // MQTT last will retain (0 or 1)
+//************* CREATE CONNECTIONS ***********************************************************************
+WiFiClient espClient;                                                           // Create wifi
+PubSubClient client(espClient);                                                 // Create MQTT client
+//************* RECONNECT MQTT ***************************************************************************
+void mqttConnect() {
+  while (!client.connected()) {                                                 // Loop until reconnected
+    if (client.connect(
+      DEVICE_HOSTNAME, MQTT_USERNAME, MQTT_PASSWORD,
+      MQTT_WILL_TOPIC, MQTT_WILL_QOS, MQTT_WILL_RETAIN,
+      MQTT_WILL_MESSAGE_OFF)) {                                                 // Connect to MQTT broker
+      client.publish(MQTT_WILL_TOPIC, MQTT_WILL_MESSAGE_ON);
+    } else {
+      delay(5000);                                                              // Wait 5 seconds before retrying
+    }
+  }
+}
+//************* SETUP ************************************************************************************
+void setup() {
+  WiFi.mode(WIFI_STA);                                                          // Switch to STA mode
+  WiFi.begin(WIFI_SSID, WIFI_PASS);                                             // Start wifi connection
+  WiFi.config(ip, gateway, subnet);                                             // Configure fixed IP
+  delay(10000);                                                                 // Give time for wifi to connect
+  client.setServer(MQTT_SERVER, MQTT_PORT);                                     // Start MQTT client
+}
+//************* LOOP *************************************************************************************
+void loop() {
+  if (!client.connected()) {                                                    // If MQTT client disconnects...
+    mqttConnect();                                                              // ...connect again
+  }
+  client.loop();                                                                // Fixes some stability issues with wifi connections
+  yield();                                                                      // Yielding
+}
+// END
